1e68f2739f78ccb79282cf69a0458f005dbf5283
tab01105 <tab01105@gmail.com>
2017-10-31 13:49:36 +0000

    予定と候補のテスト追加

--- test/test.js
+++ test/test.js
@@ -64,7 +64,8 @@ describe('/schedules', () => {
           let createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テスト候補1.*テスト候補2.*テスト候補3/)
             .expect(200)
             .end((err, res) => {
               // テストで作成したデータを削除
