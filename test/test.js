fc7187df4414ebacf7239ee97d91a6ae5c40421b
Kenji Miyata <miyatakenji0922@gmail.com>
2018-08-11 09:33:35 +0000

    add test

--- test/test.js
+++ test/test.js
@@ -65,6 +65,7 @@ describe('/schedules', () => {
           request(app)
             .get(createdSchedulePath)
             // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/${テスト予定|テストメモ1|テストメモ2|テスト候補1|テスト候補2|テスト候補3}/)
             .expect(200)
             .end((err, res) => {
               // テストで作成したデータを削除
