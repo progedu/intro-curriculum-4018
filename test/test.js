8931c5aff49b3fd532e20dd54523521a8c8c04dc
kazuki takao <kazukitakao55@gmail.com>
2018-12-02 02:01:25 +0900

    予定登録表示のテスト作成

--- test/test.js
+++ test/test.js
@@ -65,6 +65,14 @@ describe('/schedules', () => {
           request(app)
             .get(createdSchedulePath)
             // TODO 作成された予定と候補が表示されていることをテストする
+            // 上記処理でテストの予定データを作成している
+            // 取得したデータの中に登録したデータの値が含まれているかどうか確認
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
             .expect(200)
             .end((err, res) => {
               // テストで作成したデータを削除
