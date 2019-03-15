910b902b33caa14a9f250005ad17c6c04fe457bc
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2019-03-15 06:10:55 +0000

    予定の作成表示テストの追加

--- test/test.js
+++ test/test.js
@@ -64,24 +64,24 @@ describe('/schedules', () => {
           let createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)            
             .expect(200)
             .end((err, res) => {
-              if (err) return done(err);
               // テストで作成したデータを削除
-              const scheduleId = createdSchedulePath.split('/schedules/')[1];
+              let scheduleId = createdSchedulePath.split('/schedules/')[1];
               Candidate.findAll({
                 where: { scheduleId: scheduleId }
               }).then((candidates) => {
-                const promises = candidates.map((c) => { return c.destroy(); });
-                Promise.all(promises).then(() => {
-                  Schedule.findByPk(scheduleId).then((s) => { 
-                    s.destroy().then(() => { 
-                      done(); 
-                    });
-                  });
-                });
+                candidates.forEach((c) => { c.destroy(); });
+                Schedule.findById(scheduleId).then((s) => { s.destroy(); });
               });
+              if (err) return done(err);
+              done();
             });
         });
     });
