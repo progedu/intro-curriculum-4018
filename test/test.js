384df0bfa8c7f61eb452ab515fd9a816673103db
kikuchi <footer2me@gmail.com>
2019-05-18 22:11:28 +0900

    予定を表示するテストを追加

--- test/test.js
+++ test/test.js
@@ -64,7 +64,9 @@ describe('/schedules', () => {
           let createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補3/)
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
