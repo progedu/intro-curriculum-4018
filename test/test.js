a53f05c7500d12ce7bc28c0dbc3629e5a21a849a
yabuchany <yabuchany@gmail.com>
2019-10-03 22:00:35 +0900

    予定が作成でき、表示される」ことのテストの実装

--- test/test.js
+++ test/test.js
@@ -65,6 +65,12 @@ describe('/schedules', () => {
           request(app)
             .get(createdSchedulePath)
             // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
@@ -75,10 +81,10 @@ describe('/schedules', () => {
               }).then((candidates) => {
                 const promises = candidates.map((c) => { return c.destroy(); });
                 Promise.all(promises).then(() => {
-                  Schedule.findByPk(scheduleId).then((s) => { 
-                    s.destroy().then(() => { 
+                  Schedule.findByPk(scheduleId).then((s) => {
+                    s.destroy().then(() => {
                       if (err) return done(err);
-                      done(); 
+                      done();
                     });
                   });
                 });
