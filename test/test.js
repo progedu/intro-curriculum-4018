dc588fea6039664ab73aaafa0ab573913c5ed4c1
takeharu7 <memi3ktmt@gmail.com>
2020-03-03 01:01:18 +0900

    予定を作成、表示できるテストの追加

--- test/test.js
+++ test/test.js
@@ -61,10 +61,15 @@ describe('/schedules', () => {
         .expect('Location', /schedules/)
         .expect(302)
         .end((err, res) => {
-          let createdSchedulePath = res.headers.location;
+          const createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
