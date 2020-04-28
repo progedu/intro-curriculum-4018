319278c035c675967baf94aa96658ba04dc711d2
leavleaf <leavleaf@gmail.com>
2020-04-28 17:30:43 +0900

    「予定が作成でき、表示される」ことをテスト

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
-'use strict';
-let request = require('supertest');
-let app = require('../app');
-let passportStub = require('passport-stub');
-let User = require('../models/user');
-let Schedule = require('../models/schedule');
-let Candidate = require('../models/candidate');
+"use strict";
+let request = require("supertest");
+let app = require("../app");
+let passportStub = require("passport-stub");
+let User = require("../models/user");
+let Schedule = require("../models/schedule");
+let Candidate = require("../models/candidate");
 
-describe('/login', () => {
+describe("/login", () => {
   before(() => {
     passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
+    passportStub.login({ username: "testuser" });
   });
 
   after(() => {
@@ -17,35 +17,32 @@ describe('/login', () => {
     passportStub.uninstall(app);
   });
 
-  it('ログインのためのリンクが含まれる', (done) => {
+  it("ログインのためのリンクが含まれる", (done) => {
     request(app)
-      .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
+      .get("/login")
+      .expect("Content-Type", "text/html; charset=utf-8")
       .expect(/<a href="\/auth\/github"/)
       .expect(200, done);
   });
 
-  it('ログイン時はユーザー名が表示される', (done) => {
+  it("ログイン時はユーザー名が表示される", (done) => {
     request(app)
-      .get('/login')
+      .get("/login")
       .expect(/testuser/)
       .expect(200, done);
   });
 });
 
-describe('/logout', () => {
-  it('/ にリダイレクトされる', (done) => {
-    request(app)
-      .get('/logout')
-      .expect('Location', '/')
-      .expect(302, done);
+describe("/logout", () => {
+  it("/ にリダイレクトされる", (done) => {
+    request(app).get("/logout").expect("Location", "/").expect(302, done);
   });
 });
 
-describe('/schedules', () => {
+describe("/schedules", () => {
   before(() => {
     passportStub.install(app);
-    passportStub.login({ id: 0, username: 'testuser' });
+    passportStub.login({ id: 0, username: "testuser" });
   });
 
   after(() => {
@@ -53,32 +50,43 @@ describe('/schedules', () => {
     passportStub.uninstall(app);
   });
 
-  it('予定が作成でき、表示される', (done) => {
-    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+  it("予定が作成でき、表示される", (done) => {
+    User.upsert({ userId: 0, username: "testuser" }).then(() => {
       request(app)
-        .post('/schedules')
-        .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
-        .expect('Location', /schedules/)
+        .post("/schedules")
+        .send({
+          scheduleName: "テスト予定1",
+          memo: "テストメモ1\r\nテストメモ2",
+          candidates: "テスト候補1\r\nテスト候補2\r\nテスト候補3",
+        })
+        .expect("Location", /schedules/)
         .expect(302)
         .end((err, res) => {
           let createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補4/)
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
               // テストで作成したデータを削除
-              const scheduleId = createdSchedulePath.split('/schedules/')[1];
+              const scheduleId = createdSchedulePath.split("/schedules/")[1];
               Candidate.findAll({
-                where: { scheduleId: scheduleId }
+                where: { scheduleId: scheduleId },
               }).then((candidates) => {
-                const promises = candidates.map((c) => { return c.destroy(); });
+                const promises = candidates.map((c) => {
+                  return c.destroy();
+                });
                 Promise.all(promises).then(() => {
-                  Schedule.findByPk(scheduleId).then((s) => { 
-                    s.destroy().then(() => { 
+                  Schedule.findByPk(scheduleId).then((s) => {
+                    s.destroy().then(() => {
                       if (err) return done(err);
-                      done(); 
+                      done();
                     });
                   });
                 });
@@ -87,5 +95,4 @@ describe('/schedules', () => {
         });
     });
   });
-
 });
