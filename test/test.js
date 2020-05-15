6ae4e5c30535bd2c35c7066e721bb7f561d5a4d7
Kousuke Ikeda <netthanks@gmail.com>
2020-05-15 16:25:34 +0900

    予定の作成、表示のテスト

--- test/test.js
+++ test/test.js
@@ -70,7 +70,7 @@ describe('/schedules', () => {
             .expect(/テストメモ2/)
             .expect(/テスト候補1/)
             .expect(/テスト候補2/)
-            .expect(/テスト候補3/)
+            .expect(/テスト候補3/
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
