e4ac84440c03b00af778d7b968b3e59aa609d6de
henohenon <takeru_20n1100821@nnn.ed.jp>
2020-10-12 13:46:37 +0900

    テスト成功虎虎虎

--- test/test.js
+++ test/test.js
@@ -67,8 +67,13 @@ describe('/schedules', () => {
           const createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
-            .expect(200)
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト予定1/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
             .end((err, res) => {
               if (err) return done(err);
               // テストで作成したデータを削除
