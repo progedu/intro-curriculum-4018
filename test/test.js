4f724e6ca88dea402e297faa096b85abada80742
yamamoto <yamamoto2852@gmail.com>
2021-03-13 10:16:41 +0900

    作成された予定とメモと候補が表示されていることをテストする機能の実装

--- test/test.js
+++ test/test.js
@@ -1,50 +1,47 @@
-'use strict';
-let request = require('supertest');
-let app = require('../app');
-let passportStub = require('passport-stub');
-let User = require('../models/user');
-let Schedule = require('../models/schedule');
-let Candidate = require('../models/candidate');
+"use strict";
+let request = require("supertest");
+let app = require("../app");
+let passportStub = require("passport-stub");
+let User = require("../models/user");
+let Schedule = require("../models/schedule");
+let Candidate = require("../models/candidate");
 
-describe('/login', () => {
+describe("/login", () => {
   beforeAll(() => {
     passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
+    passportStub.login({ username: "testuser" });
   });
 
   afterAll(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
-  test('ログインのためのリンクが含まれる', () => {
+  test("ログインのためのリンクが含まれる", () => {
     return request(app)
-      .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
+      .get("/login")
+      .expect("Content-Type", "text/html; charset=utf-8")
       .expect(/<a href="\/auth\/github"/)
       .expect(200);
   });
-  
-  test('ログイン時はユーザー名が表示される', () => {
+
+  test("ログイン時はユーザー名が表示される", () => {
     return request(app)
-      .get('/login')
+      .get("/login")
       .expect(/testuser/)
       .expect(200);
   });
 });
 
-describe('/logout', () => {
-  test('/ にリダイレクトされる', () => {
-    return request(app)
-      .get('/logout')
-      .expect('Location', '/')
-      .expect(302);
+describe("/logout", () => {
+  test("/ にリダイレクトされる", () => {
+    return request(app).get("/logout").expect("Location", "/").expect(302);
   });
 });
 
-describe('/schedules', () => {
+describe("/schedules", () => {
   beforeAll(() => {
     passportStub.install(app);
-    passportStub.login({ id: 0, username: 'testuser' });
+    passportStub.login({ id: 0, username: "testuser" });
   });
 
   afterAll(() => {
@@ -52,35 +49,37 @@ describe('/schedules', () => {
     passportStub.uninstall(app);
   });
 
-  test('予定が作成でき、表示される', done => {
-    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+  test("予定が作成でき、表示される", (done) => {
+    User.upsert({ userId: 0, username: "testuser" }).then(() => {
       request(app)
-        .post('/schedules')
+        .post("/schedules")
         .send({
-          scheduleName: 'テスト予定1',
-          memo: 'テストメモ1\r\nテストメモ2',
-          candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3'
+          scheduleName: "テスト予定1",
+          memo: "テストメモ1\r\nテストメモ2",
+          candidates: "テスト候補1\r\nテスト候補2\r\nテスト候補3",
         })
-        .expect('Location', /schedules/)
+        .expect("Location", /schedules/)
         .expect(302)
         .end((err, res) => {
           const createdSchedulePath = res.headers.location;
           request(app)
             .get(createdSchedulePath)
-            // TODO 作成された予定と候補が表示されていることをテストする
+            .expect(/テスト予定/)
+            .expect(/テストメモ/)
+            .expect(/テスト候補/)
             .expect(200)
             .end((err, res) => {
               if (err) return done(err);
               // テストで作成したデータを削除
-              const scheduleId = createdSchedulePath.split('/schedules/')[1];
+              const scheduleId = createdSchedulePath.split("/schedules/")[1];
               Candidate.findAll({
-                where: { scheduleId: scheduleId }
-              }).then(candidates => {
-                const promises = candidates.map(c => {
+                where: { scheduleId: scheduleId },
+              }).then((candidates) => {
+                const promises = candidates.map((c) => {
                   return c.destroy();
                 });
                 Promise.all(promises).then(() => {
-                  Schedule.findByPk(scheduleId).then(s => {
+                  Schedule.findByPk(scheduleId).then((s) => {
                     s.destroy().then(() => {
                       if (err) return done(err);
                       done();
